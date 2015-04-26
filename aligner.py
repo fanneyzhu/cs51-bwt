e9d71c679064d0fa42d53519ad8b922718db2eab
fanneyzhu <fanneyzhu@college.harvard.edu>
2015-04-26 18:22:11 -0400

    find_index

--- aligner.py
+++ aligner.py
@@ -2,16 +2,13 @@ import bwt
 
 print(bwt.run_bwt("banana"))
 
-class Align(object):
-    def __init__(self, query, sequence):
-        self.query = query
+class Aligner(object):
+    def __init__(self, sequence):
         self.sequence = sequence
-
-    marker = "$"
 
     # find the first occurrence of c in sorted sequence with marker
     def occ(self, c):
-        return sorted(self.sequence+self.marker).index(c)
+        return sorted(self.sequence+bwt.marker).index(c)
 
     # find the number of occurrences of c before position i in bwt(sequence)
     def count(self, i, c):
@@ -28,13 +25,12 @@ class Align(object):
 
     # find the first and last suffix positions for the query seq
     # in the BWT transformed string
-    def bounds(self):
-
+    def bounds(self, query):
         top = 0
         bot = len(self.sequence)+1
 
         # iterate over letters in reversed query string
-        for c in self.query[::-1]:
+        for c in query[::-1]:
 
             # use LF function to map the position in the last
             # column to the position in the first column
@@ -47,12 +43,26 @@ class Align(object):
 
         return (top,bot)
         
-    # returns the index of the query string
-    # function walks left from query until $ is reached and
-    #  counts number of steps (i.e., index of query)
-    def from_start (self, idx):
-        qidx = 0
-        i = idx
-        while self.sequence[i] != "$":
-            qidx += 1
-        return qidx
+    # returns the index at which the match occurs in the query string
+    def find_index(self, query):
+        matches = self.bounds(query)
+
+        if matches == (-1, -1):
+            return None
+        else:
+            index_array = bwt.suffix_index(self.sequence+bwt.marker)
+            result = [index_array[x] for x in range(matches[0], matches[1])]
+            return result
+
+    def align(self, query):
+        matches = self.find_index(query)
+        print "Your query \"%s\" ..." % query
+        if matches == None:
+            print "does not align to the sequence \"%s\")" % self.sequence
+        else:
+            print "aligns to the sequence \"%s\" at index(es):" % self.sequence
+            print ", ".join(map(str, matches))
+
+
+andy = Aligner("banana")
+andy.align("ac")
--- bwt.py
+++ bwt.py
@@ -1,13 +1,7 @@
 # marker for the end of the string
 marker = "$"
 
-def run_bwt(str):
-    # make sure that the input string itself does not contain the marker
-    assert marker not in str, "Input cannot contain (%s)" % marker
-
-     # add marker to end of the string
-    str += "$"
-
+def suffix_index(str):
     # create sorted suffix array of string
     suffix_array = sorted(str[i:] for i in range(len(str)))
 
@@ -15,9 +9,19 @@ def run_bwt(str):
     # which the suffix occurs in the original string
     index_array = [str.index(suffix) for suffix in suffix_array]
 
+    return index_array
+
+def run_bwt(str):
+    # make sure that the input string itself does not contain the marker
+    assert marker not in str, "Input cannot contain (%s)" % marker
+
+     # add marker to end of the string
+    str += "$"
+
+    index_array = suffix_index(str)
+
     # compute bwt of string by mapping the entries of index_array to
     # the corresponding (index-1) in the original string
     result = "".join(str[index-1] for index in index_array)
 
     return result
-
